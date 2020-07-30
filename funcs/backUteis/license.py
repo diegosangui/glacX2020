92ca12a9c61ca10827624b3c479bbb31e0573d85
rafael-rfzorzi <rafaelserafim.rfzorzi@gmail.com>
2020-07-30 16:25:51 -0300

    Primeiro commit

---
+++ funcs/backUteis/license.py
@@ -0,0 +1,10 @@
+class Data_company():
+    def dados(self):
+        self.NomeEmpresa    = "Cliente teste"
+        self.NomeRuaEmp     = "Nome da Rua xxxxxxxxxx"
+        self.NomeBairroEmp  = "Nome do Bairro   xxxxxxxx"
+        self.MunicipioEmp   = "Municipio   xxxxxxxxxx"
+        self.TelefoneEmp    = "Telefone  xxxxxxxxx"
+        self.Licenca        = "Opensource Version!!!!"
+        self.LicencaCpf     = "00011100099"
+
